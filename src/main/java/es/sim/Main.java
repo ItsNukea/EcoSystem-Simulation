b43c7ec468096ef4928d93a8d77f1c522537714d
ItsNukea <keanutersteeg@outlook.com>
2026-09-04 22:16:10 +0200

    Added logging support and information class about using logback

--- src/main/java/es/sim/Main.java
+++ src/main/java/es/sim/Main.java
@@ -1,6 +1,11 @@
 package es.sim;
 
+import org.slf4j.*;
+import org.slf4j.Logger;
+
 public class Main {
+    public static Logger LOGGER = LoggerFactory.getLogger("main");
+
     static void main() {
 
     }
